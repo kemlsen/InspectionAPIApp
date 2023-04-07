397515b9349c726987dd24054a999719e079a4c8
Kemal Şen <55662927+kemlsen@users.noreply.github.com>
2023-04-07 21:58:08 +0300

    Add project files.

---
+++ InspectionAPI/Program.cs
@@ -0,0 +1,42 @@
+using InspectionAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var myAllowSpesificOrigins = "_myAllowSpesificOrigins";
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+//enable cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpesificOrigins, builder =>
+    {
+        builder.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(myAllowSpesificOrigins);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
